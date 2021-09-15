32e7009ed59965e627321ffaf8d6e42f21616042
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2021-09-15 11:39:39 +0200

    Added a setup bash script

---
+++ setup.bash
@@ -0,0 +1,14 @@
+# create catkin workspace
+cd ~/
+mkdir -p migrave_ws/src
+cd ~/migrave_ws/src && catkin_init_workspace
+
+# pull components
+cd ~/migrave_ws
+wstool init src
+wstool merge -t src https://raw.githubusercontent.com/migrave/migrave_software_setup/main/components.rosinstall
+wstool update -t src
+rosdep install --from-paths src --ignore-src --rosdistro=noetic -y
+
+# build all packages
+catkin build
